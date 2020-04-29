0c601a01353e67d36574107a7269d3d395b81d34
danie <danie@DESKTOP-LEE1SPF>
2020-04-28 23:44:16 -0400

    El pastel es una mentira

---
+++ index.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="Css/Style.css">
+        <title>ApertureScience</title>
+    </head>
+    <body>
+        <header>
+            <div class="barra">
+                <img src="image\logo.jpg">
+                </a>
+                <div class="navegacion">
+                    <nav class="Superior">
+                        <a href="index.php">Home</a>
+                        <a href="/">Nosotros</a>
+                        <a href="/">Contacto</a>
+                        <a href="/">Productos</a>
+                    </nav>
+                </div>
+            </div>
+            <div class="Slogan">
+                <h1>
+                    Innovacion para la humanidad
+                </h1>
+            </div>
+        </header>
+        
+        <section>
+            <div class="Producto">
+                <h2>Mas sobre nosotros</h2>>
+
+                <img src="image/icono1.svg" alt="Icono de Seguridad">
+                <h3>Seguridad</h3>
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sapiente, ab? In sit natus labore eum quod tenetur, praesentium voluptatem assumenda est, aut temporibus modi dolore distinctio quia voluptates, molestias placeat!</p>
+            </div>
+            <div class="Producto">
+                <img src="image/icono2.svg" alt="Mejor Precio">
+                <h3>El mejor precio</h3>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste iusto aperiam sit perferendis libero dolor maxime tempore cumque debitis ipsam laboriosam laudantium nihil autem, a odio facere, nobis aliquam beatae?</p>
+
+            </div>
+            <div class="Producto">             
+                <img src="image/icono3.svg" alt="Icono a tiempo">
+                <h3>A tiempo</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dignissimos, beatae voluptatum! Quisquam maiores ex consectetur neque pariatur corrupti, dolores, dolorem in at doloremque iste! At iste deserunt dolorum eaque minus.</p>
+                
+            </div>    
+                
+        </section>
+        <aside>
+            div
+            <h2>Otros sitios que te podrian interesar: </h2>
+            <a href="/">Black Mesa</a>
+            <a href="/">Gordon Freeman</a>
+            <a href="/">G-Man</a>
+        </aside>
+    </body>
+    <footer>
+        <div>
+        Aperture Science todos los derechos reservados 
+        </div>
+        <div>
+        Direccion: Nobody#1663 
+        </div>
+        <div>
+        Fono:+44 7480728290
+        </div>
+    </footer>
+</html>
